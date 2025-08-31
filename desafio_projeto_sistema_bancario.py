ecc841f5f30afa9d2049c2d0c6169ab3b2666871
ARSS-1 <asaferuben@outlook.com>
2025-08-31 03:17:31 -0300

    Update desafio_projeto_sistema_bancario.py
    Add arquivo com POO

--- desafio_projeto_sistema_bancario.py
+++ desafio_projeto_sistema_bancario.py
@@ -1,64 +1,134 @@
-menu = """
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[4] Sair
+from abc import ABC,abstractmethod
+from datetime import date, datetime
 
-=> """
+class Historico():
+    def __init__(self):
+        self.__transacoes = []
 
-saldo = 0
-LIMITE = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    @property
+    def extrato(self):
+        return self.__transacoes
 
-while True:
+    def adicionar_transacao(self, transacao: Transacao):
+        self.__transacoes.append(
+            {
+            "Tipo": transacao.__class__.__name__,
+            "Valor": transacao.valor,
+            "Data": datetime.now().strftime("%d-%m-%y %H:%M:%S")
+            }
+        )
 
-    opcao = int(input(menu))
+class Conta():
 
-    if opcao == 1:
-        deposito = float(input("Valor do depósito: "))
-
-        if deposito >= 0:
-            saldo += deposito
-            extrato += f"Depósito: R$ {deposito:.2f}\n"
-            print(f"Depósito de R$ {deposito:.2f} realizado com sucesso!")
-
-        else:
-            print("Valor inválido. Recomece a operação.")
+    def __init__(self, cliente: Cliente, numero:int):
+        self.__cliente = cliente
+        self.__numero = numero
+        self.__saldo = 0.0
+        self.__agencia = "0001"
+        self.__historico = Historico()
     
-    elif opcao == 2:
+    @classmethod
+    def nova_conta(cls, cliente: Cliente, numero:int) -> Conta:
+        return cls(cliente, numero)
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def extrato(self):
+        return self.__historico.extrato
+    
+    def adicionar_transacao(self, transacao: Transacao):
+        self.__historico.adicionar_transacao(transacao)
+    
+    def sacar(self, valor:float):
 
-        if numero_saques == LIMITE_SAQUES:
-            print("Limite de saques diários excedido. Tente novamente amanhã.")
-            continue
-            
-        saque = float(input("Valor do saque: "))
-
-        if saque > saldo:
+        if valor > self.__saldo:
             print("Saldo insuficiente. Recomece a operação.")
-        
-        elif saque > LIMITE:
-            print("Limite por operação excedido. O limite por operação é de R$ 500,00.")
-        
-        elif saque < 0:
-            print("Valor inválido. Recomece a operação.")
-        
+            return False
+        elif valor <= 0:
+            print("Valor inválido. Recomece a Operação.")
+            return False
         else:
-            saldo -= saque
-            numero_saques += 1
-            extrato += f"Saque: R$ {saque:.2f}\n"
-            print("Saque realizado com sucesso!")
+            self.__saldo -= valor
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+            return True
 
-    elif opcao == 3:
-        print(f" EXTRATO ".center(27, "*"))
-        print("Não foram realizadas movimentações." if not extrato else extrato) # ADICIONADO APÓS A RESOLUÇÃO
-        print(f"\nSaldo: R$ {saldo:.2f}")
+    def depositar(self, valor:float):
+
+        if valor > 0:
+            self.__saldo += valor
+            print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
+            return True
+        else:
+            print("Erro ao realizar o depósito. Recomece a operação.")
+            return False
+
+class ContaCorrente(Conta):
+    LIMITE_SAQUES = 3
+
+    def __init__(self, cliente: Cliente, numero: int, limite: float):
+        super().__init__(cliente, numero)
+        self.limite = limite
+
+class Cliente():
+    def __init__(self, endereco: str):
+        self.endereco = endereco
+        self.__contas = []
+    
+    def realizar_transacao(self, conta: Conta, transacao: Transacao):
+        transacao.registrar(conta)
+    
+    def adicionar_conta(self, conta: Conta):
+        self.__contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome: str, cpf: str, data_nascimento: date, endereco: str):
+        super().__init__(endereco)
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+    
+
+class Transacao(ABC):
+
+    @abstractmethod
+    def registrar(self, conta: Conta):
+        pass
+
+    @property
+    @abstractmethod
+    def valor(self):
+        pass
+
+class Saque(Transacao):
+    def __init__(self, valor: float):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
 
     
-    elif opcao == 4:
-        print("Obrigado por usar nossos serviços!")
-        break
+    def registrar(self, conta: Conta):
+        registro = conta.sacar(self.__valor)
 
-    else:
-        print("Operação inválida. Selecione novamente a operação desejada.")
+        if registro:
+            conta.adicionar_transacao(self)
+
+class Deposito(Transacao):
+    def __init__(self, valor: float):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+    
+    def registrar(self, conta: Conta):
+        registro = conta.depositar(self.__valor)
+
+        if registro:
+            conta.adicionar_transacao(self)
+
+    print("Obrigado por usar nossos serviços!")
